f11c042bf270087bbb92a6bcaf873f065a7b971f
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-05-06 16:43:03 -0400

    Update 1-create-database.sql

--- 1-create-database.sql
+++ 1-create-database.sql
@@ -1,3 +1,7 @@
 CREATE DATABASE trendyStore;
 CREATE DATABASE toyStore;
 
+-- 1.	Make sure you are on psql -U postgres
+-- 2.Go back into command line and do CREATE DATABASE;
+-- copy the schema you have above into psql and run, and it will create the database in sql13
+-- 3.   \c todo_database;
