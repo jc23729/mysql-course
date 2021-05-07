7a974aa59dec9fff3b2aee1b9cfc65c5e488790a
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-05-07 12:28:05 -0400

    Update 2-create-table.sql

--- 2-create-table.sql
+++ 2-create-table.sql
@@ -1,21 +1,31 @@
-use trendyStore;
-
-CREATE TABLE users(
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  f_name VARCHAR(30) NOT NULL,
-  l_name VARCHAR(30) NOT NULL,
-  email VARCHAR(60) NOT NULL,
-  phone VARCHAR(20) NOT NULL,
-  birthdate DATE NOT NULL
+CREATE TABLE IF NOT EXISTS \"users\" (
+	\"id\" serial,
+	\"f_name\" text,
+	\"l_name\" text,
+	\"email\" text,
+	\"phone\" numeric(9,2),
+	\"birthdate\" date,
+	
 );
 
-CREATE TABLE products(
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  manufacturer VARCHAR(30) NOT NULL,
-  quantity INT UNSIGNED NOT NULL,
-  phone VARCHAR(20) NOT NULL,
-  birthdate DATE NOT NULL
-);
 
-show tables;
+-- use trendystore;
+
+-- CREATE TABLE [IF NOT EXISTS] users(
+--   user_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   f_name VARCHAR(30) NOT NULL,
+--   l_name VARCHAR(30) NOT NULL,
+--   email VARCHAR(60) NOT NULL,
+--   phone VARCHAR(20) NOT NULL,
+--   birthdate DATE NOT NULL
+-- );
+
+-- CREATE TABLE [IF NOT EXISTS] products(
+--   user_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   title VARCHAR(30) NOT NULL,
+--   manufacturer VARCHAR(30) NOT NULL,
+--   quantity INT UNSIGNED NOT NULL,
+--   phone VARCHAR(20) NOT NULL,
+--   birthdate DATE NOT NULL
+-- );
+
